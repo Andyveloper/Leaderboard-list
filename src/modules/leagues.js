499ab4899c5f23ac61bde2ed92fd681b3212bb66
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-06-16 12:31:01 -0500

    Add league tiers

---
+++ src/modules/leagues.js
@@ -0,0 +1,46 @@
+const leagues = [
+  {
+    name: 'Bronze',
+    id: 1,
+    icon: 'https://liquipedia.net/commons/images/c/cb/BronzeMedium.png',
+    maxScore: 1000
+  },
+  {
+    name: 'Silver',
+    id: 2,
+    icon: 'https://liquipedia.net/commons/images/2/22/SilverMedium.png',
+    maxScore: 2000
+  },
+  {
+    name: 'Gold',
+    id: 3,
+    icon: 'https://liquipedia.net/commons/images/5/55/GoldMedium.png',
+    maxScore: 3000
+  },
+  {
+    name: 'Platinum',
+    id: 4,
+    icon: 'https://liquipedia.net/commons/images/2/2b/PlatinumMedium.png',
+    maxScore: 4000
+  },
+  {
+    name: 'Diamond',
+    id: 5,
+    icon: 'https://liquipedia.net/commons/images/9/90/DiamondMedium.png',
+    maxScore: 5000
+  },
+  {
+    name: 'Master',
+    id: 6,
+    icon: 'https://liquipedia.net/commons/images/2/26/MasterMedium.png',
+    maxScore: 6000
+  },
+  {
+    name: 'Grandmaster',
+    id: 7,
+    icon: 'https://liquipedia.net/commons/images/7/75/GrandmasterMedium.png',
+    maxScore: 7000
+  }
+]
+
+export default leagues
