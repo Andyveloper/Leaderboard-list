33589b6ad14596d77981111edc84dce63bdb48cc
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-06-17 18:15:54 -0500

    Fix first line of list

--- src/modules/refreshButton.js
+++ src/modules/refreshButton.js
@@ -7,7 +7,17 @@ const listContainer = document.getElementById('ranking');
 const refreshPage = () => {
   refreshBtn.addEventListener('click', async (e) => {
     e.preventDefault();
-    listContainer.innerHTML = '';
+    listContainer.innerHTML = `            
+    <li class="list-container__list-ranking-element--static">
+    <span class="list-container__list-ranking-element-name"
+      >Name</span
+    >
+    <span class="list-container__list-ranking-element-score"
+      >Score</span
+    >
+    <span class="list-container__list-ranking-element-league"
+      >League</span
+    >`;
     const scoreData = await getScores();
     scoreData.forEach((element) => {
       createListElement(element.user, element.score, leagueTrack(element.score));
